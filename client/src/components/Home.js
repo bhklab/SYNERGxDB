fd784bd9218b9a249ff517d85ec406c1f98ad9f1
Denis Tkachuk <deventorum@gmail.com>
2019-06-07 11:20:05 -0400

    Integrated React-Router

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
-import logo from '../logo.svg';
 import '../styles/Home.css';
 
 class Home extends Component {
