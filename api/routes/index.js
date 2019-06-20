fec204bdad270f52eada1f7623ede9c2fc7ae829
Denis Tkachuk <deventorum@gmail.com>
2019-06-20 12:31:52 -0400

    Finished building knex query for drug combo results

--- api/routes/index.js
+++ api/routes/index.js
@@ -59,15 +59,45 @@ router.post('/getCombos', (req, res, next) => {
   const drug1 = drugId1 < drugId2 ? drugId1 : drugId2;
   const drug2 = drugId1 < drugId2 ? drugId2 : drugId1;
 
-
+  // Subquery to link combo designs to respective synergy scores
   function subqueryCD() {
-    this.select('idCombo_Design')
+    this.select('*')
       .from('Combo_Design')
       .where({ idSample: sample, idDrugA: drug1, idDrugB: drug2 })
       .as('CD');
   }
-  db.select('bliss', 'loewe', 'hsa', 'zip', 'idSource')
-    .from(subqueryCD).join('Synergy_Score', 'CD.idCombo_Design', '=', 'Synergy_Score.idSynergy_Score')
+  // Subquery to get cell line name(s) and tissue name
+  function subqueryS() {
+    this.select('idCombo_Design', 'idDrugA', 'idDrugB', 'name as sampleName', 'tissue')
+      .from(subqueryCD)
+      .join('Sample', 'CD.idSample', '=', 'Sample.idSample')
+      .as('S');
+  }
+  // Subquery to get idDrugA name(s)
+  function subqueryD1() {
+    this.select('idCombo_Design', 'name as drugNameA', 'idDrugB', 'sampleName', 'tissue')
+      .from(subqueryS)
+      .join('Drug', 'S.idDrugA', '=', 'Drug.idDrug')
+      .as('D1');
+  }
+  // Subquery to get idDrugB name(s)
+  function subqueryD2() {
+    this.select('idCombo_Design', 'drugNameA', 'name as drugNameB', 'sampleName', 'tissue')
+      .from(subqueryD1)
+      .join('Drug', 'D1.idDrugB', '=', 'Drug.idDrug')
+      .as('D2');
+  }
+  // Links synergy scores to existing data
+  function subquerySS() {
+    this.select('bliss', 'loewe', 'hsa', 'zip', 'idSource', 'sampleName', 'drugNameA', 'drugNameB', 'tissue')
+      .from(subqueryD2)
+      .join('Synergy_Score', 'D2.idCombo_Design', '=', 'Synergy_Score.idSynergy_Score')
+      .as('SS');
+  }
+  // Adds source name to the results and sends it to the client
+  db.select('bliss', 'loewe', 'hsa', 'zip', 'name as sourceName', 'sampleName', 'drugNameA', 'drugNameB', 'tissue')
+    .from(subquerySS)
+    .join('Source', 'SS.idSource', '=', 'Source.idSource')
     .then((data) => {
       res.json(data);
     });
