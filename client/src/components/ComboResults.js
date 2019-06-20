a1da193e9478b4036fc02b191ff4ad0485cc6f3a
Denis Tkachuk <deventorum@gmail.com>
2019-06-20 17:30:51 -0400

    Added table to display synergy results

--- client/src/components/ComboResults.js
+++ client/src/components/ComboResults.js
@@ -1,19 +1,23 @@
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 // import styled from 'styled-components';
 // import colors from '../styles/colors';
 // import transitions from '../styles/transitions';
+
+import ResultRow from './ResultRow';
 
 
 class ComboResults extends Component {
   constructor() {
     super();
     this.state = {
-      results: []
+      results: [],
+      selected: {},
     };
   }
 
   componentDidMount() {
-    const { sample, drugId1, drugId2} = this.props
+    const { sample, drugId1, drugId2 } = this.props;
 
     fetch('/api/getCombos', {
       method: 'POST',
@@ -23,21 +27,40 @@ class ComboResults extends Component {
       },
       body: JSON.stringify({
         sample,
-        drugId1, 
-        drugId2
+        drugId1,
+        drugId2,
       }),
     })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      this.setState({results: data})
-    })
-
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ results: data });
+      });
   }
 
   render() {
-   
-    return <div>Table</div>
+    const { results } = this.state;
+    const resultRows = results.map(synergyResult => <ResultRow synergyResult={synergyResult} />);
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Tissue</th>
+            <th>Cell line</th>
+            <th>Drug A</th>
+            <th>Drug B</th>
+            <th>ZIP</th>
+            <th>Bliss</th>
+            <th>Loewe</th>
+            <th>HSA</th>
+            <th>Source</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultRows}
+        </tbody>
+      </table>
+    );
   }
 }
 
---
+++ client/src/components/ResultRow.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+// import styled from 'styled-components';
+// import colors from '../styles/colors';
+// import transitions from '../styles/transitions';
+
+
+class ResultRow extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: false,
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    console.log(this.props);
+    const {
+      tissue, sampleName, drugNameA, drugNameB, zip, bliss, loewe, hsa, sourceName,
+    } = this.props.synergyResult;
+    return (
+      <tr>
+        <td>{tissue}</td>
+        <td>{sampleName}</td>
+        <td>{drugNameA}</td>
+        <td>{drugNameB}</td>
+        <td>{zip}</td>
+        <td>{bliss}</td>
+        <td>{loewe}</td>
+        <td>{hsa}</td>
+        <td>{sourceName}</td>
+      </tr>
+    );
+  }
+}
+
+export default ResultRow;
