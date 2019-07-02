62231424f1131030a24804f6806ceb16a011ae82
Denis Tkachuk <deventorum@gmail.com>
2019-07-02 14:56:55 -0400

    Azure test hosting

---
+++ client/build/precache-manifest.9015c84283360447145142c75d1011aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365ff50bf9c0d2d0c3065e1e769b9d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7064d2f1bd5dacc9e36f",
+    "url": "/static/js/2.c8cd0ac8.chunk.js"
+  },
+  {
+    "revision": "d67f91d2bb49c0496513",
+    "url": "/static/js/main.0afd25c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e20ef990e62a8c289f60449c11fafe59",
+    "url": "/static/media/banner.e20ef990.png"
+  },
+  {
+    "revision": "edc3e907a462125ec6794ed2a3733fec",
+    "url": "/static/media/hero-image.edc3e907.jpg"
+  }
+]);
