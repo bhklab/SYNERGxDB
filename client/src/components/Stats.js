c4f44ad0c4da9a6f5c78a41f21cb3225ff0327d7
Denis Tkachuk <deventorum@gmail.com>
2019-07-03 14:22:15 -0400

    Added UI for stats

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,12 +1,27 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import CountUp from 'react-countup';
 import styled from 'styled-components';
 import colors from '../styles/colors';
+
+const StyledDiv = styled.div`
+    width: 100%    
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-around;
+    flex-wrap: wrap
+
+    &:div {
+        width: 200px;
+        min-width: 150px;
+        margin: 20px
+    }
+`;
 
 class Stats extends Component {
   constructor() {
     super();
     this.state = {
-      stats: [],
+      stats: {},
     };
   }
 
@@ -21,11 +36,63 @@ class Stats extends Component {
 
   render() {
     const { stats } = this.state;
-    return (
-      <div className="stats">
-          Stats component
-      </div>
-    );
+    if (stats.cells) {
+      return (
+        <StyledDiv className="stats">
+          <div>
+            <h2>Cell Lines</h2>
+            <h2>
+              <CountUp
+                start={0}
+                end={stats.cells}
+                duration={3}
+              />
+            </h2>
+          </div>
+          <div>
+            <h2>Tissues</h2>
+            <h2>
+              <CountUp
+                start={0}
+                end={stats.tissues}
+                duration={3}
+              />
+            </h2>
+          </div>
+          <div>
+            <h2>Drug Combinations</h2>
+            <h2>
+              <CountUp
+                start={0}
+                end={stats.combos}
+                duration={3}
+              />
+            </h2>
+          </div>
+          <div>
+            <h2>Experiments</h2>
+            <h2>
+              <CountUp
+                start={0}
+                end={stats.experiments}
+                duration={3}
+              />
+            </h2>
+          </div>
+          <div>
+            <h2>Datapoints</h2>
+            <h2>
+              <CountUp
+                start={0}
+                end={stats.datapoints}
+                duration={3}
+              />
+            </h2>
+          </div>
+        </StyledDiv>
+      );
+    }
+    return null;
   }
 }
 
