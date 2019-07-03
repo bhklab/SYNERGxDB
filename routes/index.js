ba8a00bba965463b5889540fc9b68c3b60a6017f
Denis Tkachuk <deventorum@gmail.com>
2019-07-03 11:01:34 -0400

    Added knex requests for stats

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,61 @@ const router = express.Router();
 
 
 router.get('/stats', (req, res) => {
-  // Get num of cell lines and tissues
-  // db('Sample')
-  //   .count('idSample as num_cells')
-  //   .countDistinct('tissue as num_tissue')
-  //   .then((cellList) => {
-  //     res.json(cellList);
-  //   });
-  db('combo_design')
-    .countDistinct('idDrugA', 'idDrugB as num_combos')
-    .then((cellList) => {
-      res.json(cellList);
-    });
+  const responseObject = {};
+  const getCellsAndTissues = new Promise((resolve, reject) => {
+    db('Sample')
+      .count('idSample as cells')
+      .countDistinct('tissue as tissues')
+      .then((data) => {
+        const { cells, tissues } = data[0];
+        responseObject.cells = cells;
+        responseObject.tissues = tissues;
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  const getCombinations = new Promise((resolve, reject) => {
+    db.raw('select count(distinct idDrugA, idDrugB) as combos from combo_design')
+      .then((rawData) => {
+        responseObject.combos = rawData[0][0].combos;
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  const getExperiments = new Promise((resolve, reject) => {
+    db('Combo_design')
+      .count('idCombo_Design as experiments')
+      .then((data) => {
+        responseObject.experiments = data[0].experiments;
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  const getDatapoints = new Promise((resolve, reject) => {
+    db('Combo_matrix')
+      .count('idCombo_Matrix as datapoints')
+      .then((data) => {
+        responseObject.datapoints = data[0].datapoints;
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  // Process all database requests in parallel
+  const databaseQueries = [getCellsAndTissues, getCombinations, getExperiments, getDatapoints];
+  return Promise.all(databaseQueries)
+    .then(() => {
+      console.log(responseObject);
+      res.json(responseObject);
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/getCellLines', (req, res) => {
