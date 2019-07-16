5aa07a99d542734e19f479db4c38fb82229e8d4b
Denis Tkachuk <deventorum@gmail.com>
2019-07-16 11:27:31 -0400

    Adding prop types to the plot

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import TopNav from './TopNav';
-import SideNav from './SideNav';
-// import heroImg from '../images/hero-image.jpg';
 import heroImg from '../images/bg.jpg';
 import colors from '../styles/colors';
 import transitions from '../styles/transitions';
--- client/src/components/Plots/CumulativeDensity.js
+++ client/src/components/Plots/CumulativeDensity.js
@@ -1,60 +1,92 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import Plot from 'react-plotly.js';
 
 import colors from '../../styles/colors';
 
-export default class BiomarkerPlot extends React.Component {
-  // Defining initial state and layout
+const generateDataset = (factor) => {
+  const output = [];
+  for (let i = 0; i < 500; i++) {
+    output[i] = Math.random() * factor;
+  }
+  return output;
+};
+
+class CumulativeDensity extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      bliss:
-        {
-          x: [1, 2, 3],
-          type: 'histogram',
-          cumulative: { enabled: true },
-          marker: { color: colors.color_main_2 },
+      bliss: {},
+      loewe: {},
+      hsa: {},
+      zip: {},
+      layout: {
+        height: 450,
+        paper_bgcolor: colors.trans_color_main_3,
+        plot_bgcolor: colors.trans_color_main_3,
+        yaxis: { title: 'Cumulative density' },
+        xaxis: { title: 'Synergy Score (Cell Lines, N=60)' },
+        barmode: 'overlay',
+        font: {
+          size: 16,
+          color: '#000000',
+          family: 'Raleway',
         },
+        title: {
+          text: `${props.drug1} * ${props.drug2}`,
+          size: 18,
+        },
+      },
+    };
+  }
+
+  // Methods called on loading
+  componentDidMount() {
+    this.setState({
+      bliss: {
+        x: generateDataset(1),
+        name: 'Bliss',
+        opacity: 0.5,
+        type: 'histogram',
+        cumulative: { enabled: true },
+        marker: { color: colors.color_main_2 },
+      },
       loewe:
         {
-          x: [1, 2, 3],
+          x: generateDataset(0.5),
+          name: 'Loewe',
+          opacity: 0.5,
           type: 'histogram',
           cumulative: { enabled: true },
           marker: { color: colors.color_main_3 },
         },
       hsa:
         {
-          x: [1, 2, 3],
+          x: generateDataset(1.8),
+          name: 'HSA',
           type: 'histogram',
+          opacity: 0.5,
           cumulative: { enabled: true },
           marker: { color: colors.color_main_4 },
         },
       zip:
         {
-          x: [1, 2, 3],
+          x: generateDataset(1.3),
           type: 'histogram',
+          name: 'ZIP',
+          opacity: 0.5,
           cumulative: { enabled: true },
           marker: { color: colors.color_main_5 },
         },
-    };
-  }
-
-  // Methods called on loading
-  componentDidMount() {
-
+    });
   }
 
   // Render this compoenent
   render() {
     const {
-      bliss, loewe, hsa, zip,
+      bliss, loewe, hsa, zip, layout,
     } = this.state;
     const data = [bliss, loewe, hsa, zip];
-    const layout = {
-      width: 320, height: 240, title: 'A Fancy Cumulative Plot',
-    };
     return (
       <div className="cumulative-container">
         <Plot data={data} layout={layout} graphDiv="graph" config={{ responsive: true }} />
@@ -62,3 +94,10 @@ export default class BiomarkerPlot extends React.Component {
     );
   }
 }
+
+CumulativeDensity.propTypes = {
+  drug1: PropTypes.string.isRequired,
+  drug2: PropTypes.string.isRequired,
+};
+
+export default CumulativeDensity;
