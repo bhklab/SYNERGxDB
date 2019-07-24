02d76b4d78153727b8902917f28f922ffc4e360a
Denis Tkachuk <deventorum@gmail.com>
2019-07-24 11:30:26 -0400

    Added some fixes to the cumulative density plot

--- client/src/components/Plots/CumulativeDensity.js
+++ client/src/components/Plots/CumulativeDensity.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Plot from 'react-plotly.js';
@@ -27,7 +28,7 @@ class CumulativeDensity extends React.Component {
     const {
       drug1, drug2, comboId, sample, idSource,
     } = this.props;
-    const scatterSize = 5;
+    const scatterSize = 10;
 
     fetch(`/api/combos?drugId1=${drug1.idDrug}&drugId2=${drug2.idDrug}&dataset=${idSource}`, {
       method: 'GET',
@@ -38,23 +39,26 @@ class CumulativeDensity extends React.Component {
     })
       .then(response => response.json())
       .then((comboData) => {
-        const { length } = comboData;
-
         const generateCoordinates = (data, type) => {
           const outputCoordinates = {};
-          const rawData = data;
-          const sortedData = rawData.sort((a, b) => (a[type] > b[type] ? 1 : -1));
+          const rawData = data.filter(item => (item[type] !== null));
+          const sortedData = rawData.sort((a, b) => (a[type] > b[type] ? 1 : a[type] < b[type] ? -1 : 0));
           let value = null;
-
           sortedData.some((item, index) => {
+            console.log(index, item, type);
             if (item.comboId === comboId) {
               value = item[type];
               outputCoordinates.x = item[type];
+              console.log('-----------Item 1-------------', index, item);
             }
             // Checks for last item that had same synergy score to define y axis
             // value !== null instead of value is used to avoid scenerios when value might be zero
             if (value !== null && value !== item[type]) {
-              outputCoordinates.y = (index) / (length - 1);
+              console.log('Item 2', index, item);
+              console.log('------------------------');
+              console.log('Raw data lenght is ', data.length);
+              console.log('SortedData.lenght is ', sortedData.length);
+              outputCoordinates.y = (index) / (sortedData.length - 1);
               return true;
             }
             return false;
@@ -86,6 +90,7 @@ class CumulativeDensity extends React.Component {
             // showlegend: false,
             name: `${sample} Bliss`,
             marker: { color: colors.color_main_2, size: scatterSize },
+            showlegend: false,
             mode: 'markers',
             type: 'scatter',
             legendgroup: 'bliss',
@@ -163,7 +168,7 @@ class CumulativeDensity extends React.Component {
             paper_bgcolor: colors.trans_color_main_3,
             plot_bgcolor: colors.trans_color_accent_2,
             yaxis: { title: 'Cumulative density' },
-            xaxis: { title: `Synergy Score (Cell Lines, N=${length})` },
+            xaxis: { title: `Synergy Score (Cell Lines, N=${comboData.length})` },
             barmode: 'overlay',
             font: {
               size: 16,
