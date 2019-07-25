7b2935c10b66d864afd940fa1dcbc55a7e8b3f1d
Denis Tkachuk <deventorum@gmail.com>
2019-07-25 17:42:59 -0400

    Setting up autofocus for the plot

--- client/src/components/Plots/Plot3D.js
+++ client/src/components/Plots/Plot3D.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-nested-ternary */
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Plot from 'react-plotly.js';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 import colors from '../../styles/colors';
 
-const types = ['Bliss', 'Loewe', 'HSA', 'ZIP'];
+const PlotlyContainer = styled.div`
+    min-height: 50vh;
+`;
+
+const types = [
+  { name: 'Bliss', color: colors.color_main_2 },
+  { name: 'Loewe', color: colors.color_main_1 },
+  { name: 'HSA', color: colors.color_main_4 },
+  { name: 'ZIP', color: colors.color_main_5 },
+];
 
 
 class Plot3D extends React.Component {
@@ -16,7 +25,7 @@ class Plot3D extends React.Component {
       data: [],
       layout: {
         title: {
-          text: types[props.type - 1],
+          text: types[props.type - 1].name,
           size: 18,
         },
         type: 'surface',
@@ -24,9 +33,9 @@ class Plot3D extends React.Component {
         //   camera: { eye: { x: 1.87, y: 0.88, z: -0.64 } },
           xaxis: { title: `${props.drug1.name}` },
           yaxis: { title: `${props.drug2.name}` },
-          zaxis: { title: types[props.type - 1] },
+          zaxis: { title: types[props.type - 1].name },
         },
-        autosize: true,
+        // autosize: true,
         margin: {
           l: 65,
           r: 50,
@@ -37,29 +46,13 @@ class Plot3D extends React.Component {
         plot_bgcolor: colors.trans_color_accent_2,
       },
     };
+    this.focusOnPlot = React.createRef();
   }
 
   // Methods called on loading
   componentDidMount() {
-    const { data } = this.props;
+    const { data, type } = this.props;
     console.log(data);
-    // const zData = [
-    //   [8.83, 8.89, 8.81, 8.87, 8.9, 8.87],
-    //   [8.89, 8.94, 8.85, 8.94, 8.96, 8.92],
-    //   [8.84, 8.9, 8.82, 8.92, 8.93, 8.91],
-    //   [8.79, 8.85, 8.79, 8.9, 8.94, 8.92],
-    //   [8.79, 8.88, 8.81, 8.9, 8.95, 8.92],
-    //   [8.8, 8.82, 8.78, 8.91, 8.94, 8.92],
-    //   [8.75, 8.78, 8.77, 8.91, 8.95, 8.92],
-    //   [8.8, 8.8, 8.77, 8.91, 8.95, 8.94],
-    //   [8.74, 8.81, 8.76, 8.93, 8.98, 8.99],
-    //   [8.89, 8.99, 8.92, 9.1, 9.13, 9.11],
-    //   [8.97, 8.97, 8.91, 9.09, 9.11, 9.11],
-    //   [9.04, 9.08, 9.05, 9.25, 9.28, 9.27],
-    //   [9, 9.01, 9, 9.2, 9.23, 9.2],
-    //   [8.99, 8.99, 8.98, 9.18, 9.2, 9.19],
-    //   [8.93, 8.97, 8.97, 9.18, 9.2, 9.18],
-    // ];
     const zData = [
       [8.83, 8.89, 8.81, 8.87],
       [8.89, 8.94, 8.85, 8.94],
@@ -68,8 +61,6 @@ class Plot3D extends React.Component {
     ];
     const xData = [...new Set(data.map(item => item.concA))];
     const yData = [...new Set(data.map(item => item.concB))];
-    console.log(xData, yData);
-
     const dataZ = [{
       x: xData,
       y: yData,
@@ -83,28 +74,68 @@ class Plot3D extends React.Component {
           project: { z: true },
         },
       },
-      colorscale: [[0, 'rgb(25,255,255)'], [1, 'rgb(255,0,0)']],
+      colorscale: [[0, `${colors.color_accent_1}`], [1, `${types[type - 1].color}`]],
       colorbar: {
         x: 95,
       },
     }];
 
     this.setState({ data: dataZ });
+    this.refs.focusOnPlot.focus();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { data, type } = this.props;
+    // Always check if new props are different before updating state to avoid infinite loops
+    if (type !== prevProps.type) {
+      const zData = [
+        [8.83, 8.89, 8.81, 8.87],
+        [8.89, 8.94, 8.85, 8.94],
+        [8.84, 8.9, 8.82, 8.92],
+        [8.79, 8.85, 8.79, 8.9],
+      ];
+      const xData = [...new Set(data.map(item => item.concA))];
+      const yData = [...new Set(data.map(item => item.concB))];
+
+      const dataZ = [{
+        x: xData,
+        y: yData,
+        z: zData,
+        type: 'surface',
+        contours: {
+          z: {
+            show: true,
+            usecolormap: true,
+            highlightcolor: '#42f462',
+            project: { z: true },
+          },
+        },
+        colorscale: [[0, `${colors.color_accent_1}`], [1, `${types[type - 1].color}`]],
+        colorbar: {
+          x: 95,
+        },
+      }];
+      this.setState({ data: dataZ });
+      this.refs.focusOnPlot.focus();
+      console.log(this.refs);
+    }
   }
 
   // Render this compoenent
   render() {
     const { data, layout } = this.state;
-    console.log(data);
     return (
-      <div className="cumulative-container">
-        <Plot
-          graphDiv="graph"
-          config={{ responsive: true }}
-          data={data}
-          layout={layout}
-        />
-      </div>
+      <Fragment>
+        <PlotlyContainer className="cumulative-container">
+          <Plot
+            graphDiv="graph"
+            config={{ responsive: true }}
+            data={data}
+            layout={layout}
+          />
+        </PlotlyContainer>
+        <div><span ref={this.focusOnPlot}>F</span></div>
+      </Fragment>
     );
   }
 }
