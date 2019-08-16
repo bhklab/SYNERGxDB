6f6dadd8cc81d4c63d9e4429c20e0d652d456732
Denis Tkachuk <deventorum@gmail.com>
2019-08-16 15:23:55 -0400

    Creating basic redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, compse } from 'redux';
+
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+
+// import the root reducer
+// import rootReducer from './reducers/index';
+
+
+// create an object for the default data
+const defaultState = {
+
+};
+
+const store = createStore(rootReducer, defaultState);
