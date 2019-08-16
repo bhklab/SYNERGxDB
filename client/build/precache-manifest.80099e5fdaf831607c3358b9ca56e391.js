bca300646fc434353d0563df1d42e941b653a37a
Denis Tkachuk <deventorum@gmail.com>
2019-08-16 16:51:08 -0400

    New build (most known bugs are fixed)

---
+++ client/build/precache-manifest.80099e5fdaf831607c3358b9ca56e391.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd90e2912433880a59890755dd48cfb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d461e80c89c676daa1",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "58d461e80c89c676daa1",
+    "url": "/static/js/2.15dfc5d0.chunk.js"
+  },
+  {
+    "revision": "cde0756391c2bf56de59",
+    "url": "/static/js/main.42996929.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11ae197d2e1f76b922326629fd9bd148",
+    "url": "/static/media/bg-3.11ae197d.jpg"
+  },
+  {
+    "revision": "c658c844577b14225e348174e5b4d53d",
+    "url": "/static/media/cellosaurus.c658c844.jpg"
+  },
+  {
+    "revision": "7a20b23e43a3b39bb34a61340a6f9559",
+    "url": "/static/media/drugbank.7a20b23e.svg"
+  },
+  {
+    "revision": "969ca37dd50f4f32ade36a6059c80781",
+    "url": "/static/media/logo-2.969ca37d.png"
+  },
+  {
+    "revision": "d3dce7e31c047800d2ddce15e5052244",
+    "url": "/static/media/pubmed.d3dce7e3.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e41e9db1150205faeab79d8f6cf2a5c2.js"
+  "/precache-manifest.80099e5fdaf831607c3358b9ca56e391.js"
 );
 
 self.addEventListener('message', (event) => {
