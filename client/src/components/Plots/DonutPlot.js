2207449c69f9f828fd857d3a45b71fdc29f9c356
chuntul <chantal.ho98@hotmail.com>
2019-08-29 16:53:06 -0400

    donuts now sorted by alphabetical

--- client/src/components/Plots/DonutPlot.js
+++ client/src/components/Plots/DonutPlot.js
@@ -18,10 +18,38 @@ class DonutPlot extends React.Component {
     // positions and dimensions
     const margin = {
       top: 20,
-      right: 50,
+      right: 60,
       bottom: 90,
       left: 20,
     };
+
+    // zips two arrays together, sorts increasing
+    function zip(a, b) {
+      var zipped = [];
+      
+      // zip
+      for (var i=0; i<a.length; i++) {
+        zipped.push({a: a[i], b: b[i] });
+      }
+      
+      zipped.sort(function (x, y){
+        return ((x.a < y.a) ? -1 : 1);
+      });
+      
+      var c = []
+      var d = []
+      
+      for (var i=0; i < zipped.length; i++) {
+          c[i] = zipped[i].a
+          d[i] = zipped[i].b
+      }
+      return [c,d]
+    }
+    
+    const dataSorted = zip(donutData.map(x => x.name), donutData.map(x => x.num))
+    console.log(dataSorted)
+    const names = dataSorted[0]
+    const nums = dataSorted[1]
 
     const colorPlot = d3.scaleOrdinal(['#e69a61', '#9817ff', '#18c61a', '#33b4ff', '#c9167e', '#297853', '#d7011b', '#7456c7', '#7e6276', '#afb113', '#fd879c', '#fb78fa', '#24c373', '#45bbc5', '#766b21', '#abad93', '#c19ce3', '#fd8f11', '#2f56ff', '#307a11', '#b3483c', '#0d7396', '#94b665', '#9d4d91', '#b807c8', '#086cbf', '#a2abc5', '#a35702', '#d3084b', '#8c6148', '#fa82ce', '#71be42', '#2bc0a0', '#b64064', '#d09fa2', '#daa229', '#5a6f68', '#c1aa5f', '#8943dc', '#b72ba6', '#6e629e', '#e094bf', '#dd8df2', '#c03d0b', '#7db799', '#617046', '#ff8a78', '#1263e2', '#91aaea', '#cea37e', '#9e555c', '#67b4db', '#05767b', '#537428', '#04c553', '#88b3b7', '#ff8d52', '#8abb0b', '#9b43b9', '#c83030', '#6fbc7c', '#596c83', '#926023', '#e9958d', '#a127e3', '#027b36', '#94577d', '#7543f8', '#8257ab', '#c0ab3c', '#416ca4', '#a3b444', '#b53c7e', '#ca2064', '#64c104', '#5662c0', '#c1a0c6', '#5e56e3', '#9cb37c', '#9f573b', '#65bf64', '#7e6839', '#d6a250', '#c0384a', '#75685b', '#657105', '#b1a9a9', '#ab5025', '#ea9940', '#a1a4f9', '#a9409f', '#48745a', '#dd92d5', '#c90392', '#53bcaf', '#c298f9', '#6d668a', '#aa31c0', '#e89777', '#b5ac76', '#8b5f62', '#fb85b9', '#43763f', '#38c18a', '#8ab94c', '#f87fe4', '#e196aa', '#a74b77', '#885997', '#c9a918', '#50c237', '#d50438', '#d2a267', '#acb05e', '#c1a693', '#6d696f', '#b04950', '#ba4327', '#4f7603', '#3a7282', '#2cbada', '#ed990a', '#b24c07']);
 
@@ -78,9 +106,15 @@ class DonutPlot extends React.Component {
       .append('svg:g')
       .attr('class', (d, i) => donutData[i].name);
 
-
+    let colorMap = []
     arcs.append('svg:path')
-      .attr('fill', (d, i) => colorPlot(i))
+      .attr('fill', (d, i) => {
+        colorMap.push({
+          name: d.data.name,
+          color: colorPlot(i)
+        })
+        return colorPlot(i)
+      })
       .style('opacity', 0.7)
       .attr('d', arc)
       .on('mouseover', (d) => {
@@ -93,27 +127,32 @@ class DonutPlot extends React.Component {
         tooltip.style('visibility', 'hidden');
       });
 
+    
 
     // legend
-    for (let i = 0; i < donutData.length; i++) {
+    for (let i = 0; i < names.length; i++) {
       svg.append('rect')
-        .attr('x', dims.width - (dims.width / 2 + 15))
+        .attr('x', dims.width - (dims.width / 2 + 30))
         .attr('y', i * 35 - (dims.height/2 + dims.rectY)) 
         .attr('width', 15)
         .attr('height', 15)
-        .style('fill', colorPlot(i))
+        .style('fill', () => {
+          const ind = colorMap.findIndex(item => item.name === names[i]);
+          return colorMap[ind].color
+        })
         .style('opacity', 0.7);
 
 
       svg.append('text')
-        .attr('x', dims.width - (dims.width / 2 - 5))
+        .attr('x', dims.width - (dims.width / 2 + 10))
         .attr('y', i * 35 - (dims.height / 2 + dims.textY)) 
         .attr('id', `legendLabel${donutData[i].name}`)
         .style('text-anchor', 'start')
-        .style('font-size', '14px')
+        .style('font-size', function() {if (keyName == "tissue") {return 13} else {return 14}})
+        .style("text-transform", function() {if (keyName == "tissue") {return "uppercase"} else {return "normal"}})
         .style('opacity', 1)
         .attr('fill', colors.blue_main)
-        .text(donutData[i].name);
+        .text(function() {if (keyName == "tissue") {return names[i] + ", N = " + nums[i]} else {return names[i]}});
     }
   }
 
