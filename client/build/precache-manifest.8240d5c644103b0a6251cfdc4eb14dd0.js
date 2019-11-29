d3fa7ca6a165a13f81cf7563b8a28d1f6d8d8afd
Denis Tkachuk <deventorum@gmail.com>
2019-11-29 17:07:12 -0500

    Build Nov 29 (Pharmacogenomics Page)

---
+++ client/build/precache-manifest.8240d5c644103b0a6251cfdc4eb14dd0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18116d81f777b17727b1ff7d86d4421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "605c629ab20db610fee4",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "605c629ab20db610fee4",
+    "url": "/static/js/2.2b2b20cc.chunk.js"
+  },
+  {
+    "revision": "05de2bfddb3fe5695bb3",
+    "url": "/static/js/main.110a881f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11ae197d2e1f76b922326629fd9bd148",
+    "url": "/static/media/bg-3.11ae197d.jpg"
+  },
+  {
+    "revision": "c658c844577b14225e348174e5b4d53d",
+    "url": "/static/media/cellosaurus.c658c844.jpg"
+  },
+  {
+    "revision": "4634d1be5d29335c46620af578e46ec6",
+    "url": "/static/media/drugbank.4634d1be.png"
+  },
+  {
+    "revision": "969ca37dd50f4f32ade36a6059c80781",
+    "url": "/static/media/logo-2.969ca37d.png"
+  },
+  {
+    "revision": "e637a81fb9d7f67d9b2dc00ccbdb0a6e",
+    "url": "/static/media/pubmed.e637a81f.jpeg"
+  }
+]);
