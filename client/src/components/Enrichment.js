14980be062b1c1c13cdef69890fd850af074c051
Denis Tkachuk <deventorum@gmail.com>
2019-12-10 17:19:10 -0500

    Basic architecture for tissue enrichment analysis page

--- client/src/components/Enrichment.js
+++ client/src/components/Enrichment.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import queryString from 'query-string';
-import QueryCard from './UtilComponents/QueryCard';
-// import colors from '../styles/colors';
+import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+
+// import colors from '../styles/colors';
 // import transitions from '../styles/transitions';
+import QueryCard from './UtilComponents/QueryCard';
+import DownloadButton from './UtilComponents/DownloadButton';
+import LoadingComponent from './UtilComponents/Loading';
 
 const EnrichmentDiv = styled.div`
   width: 100%;
@@ -24,20 +28,48 @@ class Enrichment extends Component {
   constructor() {
     super();
     this.state = {
-      results: [],
+      data: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
-
+    console.log('Fetch request');
   }
 
   render() {
+    const { data, loading } = this.state;
     const { location } = this.props;
     const requestParams = queryString.parse(location.search);
     const {
       sample, drugId1, drugId2, dataset,
     } = requestParams;
+
+    const headers = [
+      { displayName: 'Dataset', id: 'dataset' },
+      { displayName: 'Tissue', id: 'tissue' },
+      { displayName: 'Methods', id: 'score' },
+      { displayName: 'AUC', id: 'auc' },
+    ];
+    let filename = 'tissue_enrichment';
+    if (sample) filename = filename.concat(`_${sample}`);
+    if (dataset) filename = filename.concat(`_${dataset}`);
+    if (drugId1) filename = filename.concat(`_${drugId1}`);
+    if (drugId2) filename = filename.concat(`_${drugId2}`);
+
+    const columns = [{
+      Header: 'Dataset',
+      accessor: 'dataset',
+    }, {
+      Header: 'Tissue',
+      accessor: 'tissue',
+    }, {
+      Header: 'Methods',
+      accessor: 'score',
+    }, {
+      Header: 'AUC',
+      accessor: 'auc',
+    }];
 
     return (
       <main>
@@ -48,7 +80,21 @@ class Enrichment extends Component {
           sample={sample}
         />
         <EnrichmentDiv>
-          <h2>Enrichment Component</h2>
+          <ReactTable
+            data={data}
+            columns={columns}
+            defaultPageSize={25}
+            filterable
+            sortable
+            className="-highlight"
+            loading={loading}
+            LoadingComponent={LoadingComponent}
+          />
+          <DownloadButton
+            data={data}
+            headers={headers}
+            filename={filename}
+          />
         </EnrichmentDiv>
       </main>
     );
