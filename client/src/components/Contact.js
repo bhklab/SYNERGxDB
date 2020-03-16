c38f413c6fa816635ca039f842e2362a17b700cf
deventorum <deventorum@gmail.com>
2020-03-16 18:16:48 -0400

    Web support email address

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -33,32 +33,37 @@ const StyledWrapper = styled.div`
 `;
 
 
-class Drugs extends Component {
-  render() {
-    return (
-      <Fragment>
-          <StyledWrapper className="wrapper">
-            <h1>Contact Us</h1>
-            <div className="content">
-              <b>Email: </b> <a href='mailto: SYNERGxDB@gmail.com'>SYNERGxDB@gmail.com</a> <p/>
-              <b>BHKLAB</b> <br/>
-              The MaRS Center<br/>
-              TMDT room 11-310<br/>
-              101 College Street Toronto, ON<br/>
+const Contact = () => (
+  <Fragment>
+    <StyledWrapper className="wrapper">
+      <h1>Contact Us</h1>
+      <div className="content">
+        <b>Email: </b>
+        {' '}
+        <a href="mailto: support@synergxdb.ca">support@synergxdb.ca</a>
+        {' '}
+        <p />
+        <b>BHKLAB</b>
+        {' '}
+        <br />
+              The MaRS Center
+        <br />
+              TMDT room 11-310
+        <br />
+              101 College Street Toronto, ON
+        <br />
               M5G 1L7, Canada
-            </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.43444669756!2d-79.39085344846093!3d43.65993365990993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34b632b77689%3A0x901c210dff19e5a4!2s101%20College%20St%2C%20Toronto%2C%20ON%20M5G%201L7!5e0!3m2!1sen!2sca!4v1581544280286!5m2!1sen!2sca"></iframe>
-            
-          </StyledWrapper>
-        <footer>
-          <div className="footer-wrapper">
-            <p>Copyright © 2019. All rights reserved</p>
-          </div>
-        </footer>
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.43444669756!2d-79.39085344846093!3d43.65993365990993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34b632b77689%3A0x901c210dff19e5a4!2s101%20College%20St%2C%20Toronto%2C%20ON%20M5G%201L7!5e0!3m2!1sen!2sca!4v1581544280286!5m2!1sen!2sca" />
+
+    </StyledWrapper>
+    <footer>
+      <div className="footer-wrapper">
+        <p>Copyright © 2019. All rights reserved</p>
+      </div>
+    </footer>
+  </Fragment>
+);
 
 
-export default Drugs;
+export default Contact;
